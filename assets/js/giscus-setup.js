b4f773aa8870b1a8a96fefcd3fd9dee3294b947f
KendellHsu <57028390+KendellHsu@users.noreply.github.com>
2025-12-05 22:40:43 +0000

    Deploying to gh-pages from @ KendellHsu/kendellhsu.github.io@47e4bfa5502740ed754d050fa91aa68e183024ea 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "KendellHsu/pingjhaohsu.github.io",
+    "data-repo": "KendellHsu/kendellhsu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
